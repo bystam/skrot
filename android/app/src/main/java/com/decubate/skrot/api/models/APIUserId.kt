eb749cffe70f41583d103c0ee60dec6d30da08ee
Meidi Tönisson <meidi@noomi.com>
2019-03-17 19:14:19 +0100

    Add a bunch of nifty dependencies and some boilerplate

---
+++ android/app/src/main/java/com/decubate/skrot/api/models/APIUserId.kt
@@ -0,0 +1,3 @@
+package com.decubate.skrot.api.models
+
+data class APIUserId(val userId: String)
---
+++ android/app/src/main/java/com/decubate/skrot/api/models/APIWorkoutTemplate.kt
@@ -0,0 +1,3 @@
+package com.decubate.skrot.api.models
+
+data class APIWorkoutTemplate(val name: String, val exercises: List<APIExerciseTemplate>)
